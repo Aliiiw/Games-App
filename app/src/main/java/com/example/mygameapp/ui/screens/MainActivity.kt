69006ecc436ff0a0fb0aafdb79fb0dff2c70c23e
Ali Rahimi <1aliirahimi1@gmail.com>
2023-03-17 16:24:12 +0330

    finish

--- app/src/main/java/com/example/mygameapp/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/mygameapp/ui/screens/MainActivity.kt
@@ -6,23 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mygameapp.ui.theme.MyMovieAppTheme
+import com.example.mygameapp.ui.theme.MyGameAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyMovieAppTheme {
+            MyGameAppTheme {
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    HomeScreen()
                 }
             }
         }
